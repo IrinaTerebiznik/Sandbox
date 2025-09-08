aec275c76100bbe615fd48ff11542ab8f1515223
Irina <irina@inorbit.ai>
2025-09-08 18:34:44 -0300

    as

--- ros_tryouts/ros_ciie_ws/build.sh
+++ ros_tryouts/ros_ciie_ws/build.sh
@@ -7,5 +7,7 @@ echo ">>> Construyendo imagen Docker: $IMAGE_NAME"
 docker build -t $IMAGE_NAME .
 
 echo ">>> Imagen construida con éxito."
-echo ">>> Para correr el contenedor ejecutá:"
-echo "docker run -it --rm --privileged -v /dev:/dev -v /sys:/sys $IMAGE_NAME"
+echo
+echo ">>> Para correr el contenedor:"
+echo "1. Copiá y renombra config/example.env a config/.env y editá el puerto USB"
+echo "2. Ejecutá: docker run --env-file config/.env --device=\$USB_PORT -it --rm $IMAGE_NAME"
